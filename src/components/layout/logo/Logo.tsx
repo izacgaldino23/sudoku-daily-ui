0ecfffe194f5735539c5d1e28d119cf7f4392f89
izacgaldino23 <izacgaldino@outlook.com>
2026-02-14 12:09:44 -0300

    feat: update logo component layout and styles for improved design

--- src/components/layout/logo/Logo.tsx
+++ src/components/layout/logo/Logo.tsx
@@ -3,7 +3,14 @@ import "./Logo.scss"
 export default function Logo() {
 	return (
 		<div className="logo">
-			<h1>Daily Sudoku</h1>
+			<div className="sector">
+				<span className="square filled">D</span>
+				<span className="square"></span>
+				<span className="square"></span>
+				<span className="square filled">S</span>
+			</div>
+
+			<h2 className="title">Daily Sudoku</h2>
 		</div>
 	)
 }
