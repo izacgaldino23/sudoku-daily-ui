0ae1def823d927a33db3a62f8e20f3f4a3b66752
izacgaldino23 <izacgaldino@outlook.com>
2026-03-16 16:13:33 -0300

    feat: fix active classname

--- src/components/layout/sidebar/Sidebar.tsx
+++ src/components/layout/sidebar/Sidebar.tsx
@@ -1,11 +1,14 @@
-import { NavLink } from "react-router-dom";
+import { NavLink, useLocation } from "react-router-dom";
 import "./Sidebar.scss"
 
 export default function Sidebar() {
+	const location = useLocation();
+	const isPlayActive = location.pathname === "/" || location.pathname.startsWith("/play");
+
 	return (
 		<aside className="sidebar">
 			<nav>
-				<NavLink to="/">Play</NavLink>
+				<NavLink to="/" className={() => isPlayActive ? "active" : ""}>Play</NavLink>
 				<NavLink to="/leaderboard">Leaderboard</NavLink>
 				<NavLink to="/profile">Profile</NavLink>
 				<NavLink to="/about">About</NavLink>
